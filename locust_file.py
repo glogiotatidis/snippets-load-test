94a399e89974a8f9dfa77d691c81a98e0b8d5c31
Giorgos Logiotatidis <seadog@sealabs.net>
2016-02-23 16:13:50 +0200

    Initial commit.

---
+++ locust_file.py
@@ -0,0 +1,37 @@
+import random
+from urlparse import urljoin
+
+from locust import HttpLocust, TaskSet, task
+
+
+with open('desktop.urls') as f:
+    DESKTOP_URLS = f.read().splitlines()
+
+with open('mobile.urls') as f:
+    MOBILE_URLS = f.read().splitlines()
+
+
+class DesktopTaskSet(TaskSet):
+    @task
+    def fetch_snippets(self):
+        url = random.choice(DESKTOP_URLS)
+        with self.client.get(url, catch_response=True) as response:
+            if response.status_code == 301:
+                response.success()
+
+
+class MobileTaskSet(TaskSet):
+    @task
+    def fetch_snippets(self):
+        url = random.choice(MOBILE_URLS)
+        self.client.get(url)
+
+
+class DesktopLocust(HttpLocust):
+    weight = 5
+    task_set = DesktopTaskSet
+
+
+class MobileLocust(HttpLocust):
+    weight = 3
+    task_set = MobileTaskSet
