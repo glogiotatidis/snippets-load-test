4e6a4d03baa029512c71aa0087c9aebdcb8fdde7
Giorgos Logiotatidis <seadog@sealabs.net>
2016-03-24 17:23:09 +0200

    WAT?

--- urls/s.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-with open(sys.argv[1]) as f:
-    LINES = f.read().splitlines()
-
-for line in LINES:
-    occur, url = line.split()
-    weight = int((int(occur)/10) + 0.5)
-    print weight, url
